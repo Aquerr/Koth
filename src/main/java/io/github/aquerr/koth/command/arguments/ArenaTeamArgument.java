57697c57e01d38ea8f8126a5df66cb8206eb57c9
Aquerr <nerdianin@gmail.com>
2019-12-02 16:19:32 +0100

    Add ArenaTeamArgument

---
+++ src/main/java/io/github/aquerr/koth/command/arguments/ArenaTeamArgument.java
@@ -0,0 +1,74 @@
+package io.github.aquerr.koth.command.arguments;
+
+import io.github.aquerr.koth.Koth;
+import io.github.aquerr.koth.entity.Arena;
+import io.github.aquerr.koth.entity.ArenaTeam;
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.command.args.ArgumentParseException;
+import org.spongepowered.api.command.args.CommandArgs;
+import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.command.args.CommandElement;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.text.Text;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+public class ArenaTeamArgument extends CommandElement
+{
+	private final Koth plugin;
+
+	public ArenaTeamArgument(final Koth plugin, final @Nullable Text key)
+	{
+		super(key);
+		this.plugin = plugin;
+	}
+
+	@Nullable
+	@Override
+	protected ArenaTeam parseValue(final CommandSource source, final CommandArgs args) throws ArgumentParseException
+	{
+		while(args.hasNext())
+		{
+			final Player player = (Player) source;
+			Arena arena = this.plugin.getPlayersCreatingArena().get(player.getUniqueId());
+			if (arena == null)
+				arena = this.plugin.getPlayersEditingArena().get(player.getUniqueId());
+
+			if(arena == null)
+				throw new ArgumentParseException(Text.of("Player is not editing any arena!"), source.getName(), 0);
+
+			for(final ArenaTeam arenaTeam : arena.getTeams())
+			{
+				if(arenaTeam.getName().equals(args.next()))
+					return arenaTeam;
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public List<String> complete(final CommandSource src, final CommandArgs args, final CommandContext context)
+	{
+		if(!(src instanceof Player))
+		{
+			return new ArrayList<>();
+		}
+
+		final Player player = (Player) src;
+		Arena arena = this.plugin.getPlayersCreatingArena().get(player.getUniqueId());
+		if (arena == null)
+			arena = this.plugin.getPlayersEditingArena().get(player.getUniqueId());
+
+		final Set<ArenaTeam> teams = arena.getTeams();
+		final List<String> teamsNames = new LinkedList<>();
+		for(final ArenaTeam arenaTeam : teams)
+		{
+			teamsNames.add(arenaTeam.getName());
+		}
+		return teamsNames;
+	}
+}
